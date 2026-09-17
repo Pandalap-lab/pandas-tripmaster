4d2a83252c7ab14ff8edce59f5bb01befca2e5f5
Pandalap-lab <noname.g8@gmail.com>
2026-09-17 16:47:11 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,64 @@
-const CACHE = 'tripmaster-v14';
-const SHELL = ['/', '/?app=14', '/manifest.webmanifest', '/icon-192.png', '/icon-512.png', '/apple-touch-icon.png'];
-self.addEventListener('install', event => event.waitUntil(Promise.all([caches.open(CACHE).then(cache => cache.addAll(SHELL)), self.skipWaiting()])));
-self.addEventListener('activate', event => event.waitUntil(Promise.all([caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))), self.clients.claim()])));
+const CACHE = 'tripmaster-v14-github';
+
+const SHELL = [
+  './',
+  './?app=14',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png',
+  './apple-touch-icon.png'
+];
+
+self.addEventListener('install', event =>
+  event.waitUntil(
+    Promise.all([
+      caches.open(CACHE).then(cache => cache.addAll(SHELL)),
+      self.skipWaiting()
+    ])
+  )
+);
+
+self.addEventListener('activate', event =>
+  event.waitUntil(
+    Promise.all([
+      caches.keys().then(keys =>
+        Promise.all(
+          keys
+            .filter(key => key !== CACHE)
+            .map(key => caches.delete(key))
+        )
+      ),
+      self.clients.claim()
+    ])
+  )
+);
+
 self.addEventListener('fetch', event => {
-  if(event.request.method !== 'GET') return;
-  if(event.request.mode === 'navigate'){
-    event.respondWith(fetch(event.request).then(response => {const copy=response.clone();caches.open(CACHE).then(cache=>cache.put('/?app=14',copy));return response}).catch(()=>caches.match('/?app=14')));
+  if (event.request.method !== 'GET') return;
+
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE).then(cache => cache.put('./?app=14', copy));
+          return response;
+        })
+        .catch(() => caches.match('./?app=14'))
+    );
     return;
   }
-  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {if(response.ok){const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy))}return response})));
+
+  event.respondWith(
+    caches.match(event.request).then(cached =>
+      cached ||
+      fetch(event.request).then(response => {
+        if (response.ok) {
+          const copy = response.clone();
+          caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        }
+        return response;
+      })
+    )
+  );
 });
