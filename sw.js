959b53bc7a91272b6e7fca40d025102d6d575203
Pandalap-lab <noname.g8@gmail.com>
2026-09-18 18:16:12 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE = 'tripmaster-v15-github';
+const CACHE = 'tripmaster-v16-github';
 
 const SHELL = [
   './',
-  './?app=15',
+  './?app=16',
   './manifest.webmanifest',
   './icon-192.png',
   './icon-512.png',
@@ -41,10 +41,10 @@ self.addEventListener('fetch', event => {
       fetch(event.request)
         .then(response => {
           const copy = response.clone();
-          caches.open(CACHE).then(cache => cache.put('./?app=15', copy));
+          caches.open(CACHE).then(cache => cache.put('./?app=16', copy));
           return response;
         })
-        .catch(() => caches.match('./?app=15'))
+        .catch(() => caches.match('./?app=16'))
     );
     return;
   }
